ca58f1b036ae7e95af92618aac2af6de229a0a4f
Usman Farooq <usmanfarooq_123@hotmail.com>
2026-04-28 22:02:31 +0500

    some update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 return {
-	"navarasu/onedark.nvim",
-	config = function()
-		require("onedark").setup {
-			style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-			transparent = false, -- Show/hide background
-			term_colors = true, -- Change terminal color as per the selected theme style
-		}
+	{
+		"navarasu/onedark.nvim",
+		config = function()
+			require("onedark").setup({
+				style = "cool", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+				transparent = false, -- Show/hide background
+				term_colors = true, -- Change terminal color as per the selected theme style
+			})
 
-		require("onedark").load()
-	end
+			require("onedark").load()
+		end,
+	},
 }
